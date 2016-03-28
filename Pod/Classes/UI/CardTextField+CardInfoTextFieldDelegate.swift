d8fff6bc3de7d31a0b508690f2ce61a24497cca0
Shiyuan Jiang <James.JSY@gmail.com>
2016-03-28 14:31:23 -0700

    Minor refactor on CardInfoTextFieldDelegate

--- Pod/Classes/UI/CardTextField+CardInfoTextFieldDelegate.swift
+++ Pod/Classes/UI/CardTextField+CardInfoTextFieldDelegate.swift
@@ -11,12 +11,14 @@ import UIKit
 extension CardTextField: CardInfoTextFieldDelegate {
 
     public func textField(textField: UITextField, didEnterValidInfo: String) {
-        checkCardExpired()
+        updateNumberColor()
+        notifyDelegate()
         selectNextTextField(textField)
     }
     
     public func textField(textField: UITextField, didEnterPartiallyValidInfo: String) {
-        checkCardExpired()
+        updateNumberColor()
+        notifyDelegate()
     }
     
     private func selectNextTextField(textField: UITextField) {
@@ -27,7 +29,7 @@ extension CardTextField: CardInfoTextFieldDelegate {
         }
     }
     
-    private func checkCardExpired() {
+    private func updateNumberColor() {
         // if the date is Expiry.invalid, it means that no real date is calculated yet
         // if the calculated real date is in the past, set the text color for the date to `invalidNumberColor`
         if card.expiryDate.rawValue.timeIntervalSinceNow < 0 && card.expiryDate != Expiry.invalid {
@@ -37,7 +39,5 @@ extension CardTextField: CardInfoTextFieldDelegate {
             monthTextField?.textColor = numberInputTextField?.textColor ?? UIColor.blackColor()
             yearTextField?.textColor = numberInputTextField?.textColor ?? UIColor.blackColor()
         }
-        
-        notifyDelegate()
     }
 }
