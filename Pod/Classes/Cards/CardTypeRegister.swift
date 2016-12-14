a1214f2bf709126da0f88fbaa8e104c8b1c4857e
Daniel Vancura <daniel@prolificinteractive.com>
2016-12-14 14:07:00 -0800

    Opened up variables and functions
    To remain compatible with the implementation before Swift 3 migration, publicly declared variables should be open as far as possible as this comes closer to the semantic of public before Swift 3.

--- Pod/Classes/Cards/CardTypeRegister.swift
+++ Pod/Classes/Cards/CardTypeRegister.swift
@@ -14,10 +14,10 @@ open class CardTypeRegister {
     /**
      The default card type register, shared among all CardTextFields.
      */
-    public static let sharedCardTypeRegister = CardTypeRegister(registeredCardTypes: CardTypeRegister.defaultCardTypes)
+    open static let sharedCardTypeRegister = CardTypeRegister(registeredCardTypes: CardTypeRegister.defaultCardTypes)
     
     /// An array of all registered card types. You can edit this array with `registerCardType`, `unregisterCardType` or `setRegisteredCardTypes`.
-    public private(set) var registeredCardTypes: [CardType]
+    open private(set) var registeredCardTypes: [CardType]
     
     /**
      Creates a new `CardTypeRegister` that accepts no card types.
@@ -37,7 +37,7 @@ open class CardTypeRegister {
     }
 
     /// An array with the default card types provided by Caishen.
-    public static let defaultCardTypes: [CardType] = [
+    open static let defaultCardTypes: [CardType] = [
             AmericanExpress(),
             ChinaUnionPay(),
             DinersClub(),
@@ -52,7 +52,7 @@ open class CardTypeRegister {
      
      - parameter cardType: The card type that should be contained in this card type register.
      */
-    public func register(cardType: CardType) {
+    open func register(cardType: CardType) {
         if registeredCardTypes.contains(where: { $0.isEqual(to: cardType) }) {
             return
         }
@@ -65,7 +65,7 @@ open class CardTypeRegister {
      
      - parameter cardType: The card type that should be removed from this card type register.
      */
-    public func unregister(cardType: CardType) {
+    open func unregister(cardType: CardType) {
         registeredCardTypes = registeredCardTypes.filter { !$0.isEqual(to: cardType) }
     }
     
@@ -74,7 +74,7 @@ open class CardTypeRegister {
      
      - parameter cardTypes: The new range of card types contained in this card type register.
      */
-    public func setRegisteredCardTypes<T: Sequence>(_ cardTypes: T) where T.Iterator.Element == CardType {
+    open func setRegisteredCardTypes<T: Sequence>(_ cardTypes: T) where T.Iterator.Element == CardType {
         registeredCardTypes = [CardType]()
         registeredCardTypes.append(contentsOf: cardTypes)
     }
@@ -88,7 +88,7 @@ open class CardTypeRegister {
      
      - returns: An instance of UnknownCardType, if no card type matches the Issuer Identification Number of the provided card number or any other card type that matches the card number.
      */
-    public func cardType(for number: Number) -> CardType {
+    open func cardType(for number: Number) -> CardType {
         for i in (0...min(number.length, 6)).reversed() {
             if let substring = number.rawValue[0,i], let substringAsNumber = Int(substring) {
                 if let firstMatchingCardType = registeredCardTypes.filter({
