9786fad1c0c65507ed93756a1b61aca0660eaf55
Aleksandar Vukovic <vualeks@gmail.com>
2022-07-11 12:52:33 +0200

    t

--- Package.swift
+++ Package.swift
@@ -21,7 +21,8 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "Caishen",
-            dependencies: []),
+            dependencies: [],
+            path: "Pod/Classes"),
         .testTarget(
             name: "CaishenTests",
             dependencies: ["Caishen"])
